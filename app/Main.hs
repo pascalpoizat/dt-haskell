a7a6c96c9ba6afae04974ddab3577e102bf740c5
pascalpoizat <pascal.poizat@lip6.fr>
2018-08-20 15:54:38 +0300

    feat: add more functions in Vect

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude #-}
 module Main where
 
 import           Data.Type.Natural
 import           Data.Vect
+import           Prelude           (IO, Integer, Maybe (..), String, print,
+                                    pure, putStrLn, ($), (+), (<$>), (>>=))
 
-type IVect3 = Vect N3 Integer
+--
+-- consider writing real tests using Tasty
+--
+
+type IVect n = Vect n Integer
+type IVect3 = IVect N3
 
 aV1 :: IVect3
 aV1 = 1 :- 2 :- 3 :- Nil
@@ -11,9 +19,47 @@ aV1 = 1 :- 2 :- 3 :- Nil
 aV2 :: IVect3
 aV2 = 4 :- 5 :- 6 :- Nil
 
-main :: IO ()
-main = do
+aV3 :: IVect3
+aV3 = replicate sN3 0
+
+aV4 :: IVect3
+aV4 = replicate' 0
+
+aV5 :: Maybe (IVect N6)
+aV5 = fromList' [1,1,3,4,7,11]
+
+testAppend :: IO ()
+testAppend = print $ append aV2 aV1
+
+testFromList :: IO ()
+testFromList = case aV5 of
+  Nothing -> print "nothing"
+  Just v  -> print v
+
+testShow :: IO ()
+testShow = do
   print aV1
   print aV2
-  print $ append aV1 aV2
 
+testReplicate :: IO ()
+testReplicate = do
+  print aV3
+  print aV4
+
+testMap :: IO ()
+testMap = print $ (+ 1) <$> append aV2 aV1
+
+testSort :: IO ()
+testSort = print $ sort $ append aV2 aV1
+
+test :: String -> IO () -> IO ()
+test what f = putStrLn what >>= pure f
+
+main :: IO ()
+main = do
+  test "show" testShow
+  test "replicate" testReplicate
+  test "append" testAppend
+  test "fromList" testFromList
+  test "map" testMap
+  test "sort" testSort
--- src/Data/Vect.hs
+++ src/Data/Vect.hs
@@ -1,57 +1,71 @@
-{-# LANGUAGE DataKinds                 #-}
+-- {-# LANGUAGE ConstraintKinds           #-}
+-- {-# LANGUAGE DataKinds                 #-}
 -- {-# LANGUAGE DefaultSignatures         #-}
-{-# LANGUAGE EmptyCase                 #-}
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE FlexibleContexts          #-}
-{-# LANGUAGE FlexibleInstances         #-}
-{-# LANGUAGE GADTs                     #-}
-{-# LANGUAGE InstanceSigs              #-}
-{-# LANGUAGE KindSignatures            #-}
-{-# LANGUAGE MultiParamTypeClasses     #-}
-{-# LANGUAGE NoImplicitPrelude         #-}
-{-# LANGUAGE PolyKinds                 #-}
-{-# LANGUAGE RankNTypes                #-}
-{-# LANGUAGE ScopedTypeVariables       #-}
-{-# LANGUAGE StandaloneDeriving        #-}
+-- {-# LANGUAGE EmptyCase                 #-}
+-- {-# LANGUAGE ExistentialQuantification #-}
+-- {-# LANGUAGE FlexibleContexts          #-}
+-- {-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE GADTs              #-}
+-- {-# LANGUAGE InstanceSigs              #-}
+-- {-# LANGUAGE KindSignatures            #-}
+-- {-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE NoImplicitPrelude  #-}
+-- {-# LANGUAGE PolyKinds                 #-}
+-- {-# LANGUAGE RankNTypes                #-}
+-- {-# LANGUAGE ScopedTypeVariables       #-}
+{-# LANGUAGE StandaloneDeriving #-}
 -- {-# LANGUAGE TemplateHaskell           #-}
-{-# LANGUAGE TypeFamilies              #-}
-{-# LANGUAGE TypeInType                #-}
-{-# LANGUAGE TypeOperators             #-}
-{-# LANGUAGE UndecidableInstances      #-}
+-- {-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE TypeInType         #-}
+{-# LANGUAGE TypeOperators      #-}
+-- {-# LANGUAGE UndecidableInstances      #-}
 
 module Data.Vect
 where
 
-import           Data.Singletons.TH
 import           Data.Type.Natural
-import           Prelude            hiding (head, replicate, tail, zipWith)
+import           Prelude           hiding (head, length, map, replicate, tail,
+                                    zipWith)
 
--- TODO: find why (a :: *) does not work
+-- TODO: a should better be (a :: *)
 data Vect (size :: Nat) a where
   Nil :: Vect Z a
   (:-) :: a -> Vect n a -> Vect (S n) a
 infixr 5 :-
 
+--
+-- typeclasses
+--
+
 deriving instance Eq a => Eq (Vect n a)
-
-toList :: Vect n a -> [a]
-toList Nil      = []
-toList (a :- v) = a : toList v
-
---fromList :: SNat n -> [a] -> Maybe (Vector n a)
---fromList SZ _ = Just Nil
---fromList (SS n) (x :: xs) = (x :-) <$> fromList n xs
---fromList _ _  = Nothing
-
---fromList' :: SingI n => [a] -> Vector n a
---fromList' = fromList sing
 
 instance Show a => Show (Vect n a) where
   showsPrec p = showsPrec p . toList
 
+instance Functor (Vect n) where
+  fmap = map
+
+--
+-- constructors
+--
+
+empty :: Vect Z a
+empty = Nil
+
 append :: Vect n a -> Vect m a -> Vect (n + m) a
-append Nil v        = v
+append Nil       v  = v
 append (a :- v1) v2 = a :- append v1 v2
+
+replicate :: SNat n -> a -> Vect n a
+replicate SZ     _ = Nil
+replicate (SS n) a = a :- replicate n a
+
+replicate' :: SingI n => a -> Vect n a
+replicate' = replicate sing
+
+--
+-- destructors
+--
 
 head :: Vect (S n) a -> a
 head (a :- v) = a
@@ -59,18 +73,34 @@ head (a :- v) = a
 tail :: Vect (S n) a -> a
 tail (a :- v) = a
 
-replicate :: SNat n -> a -> Vect n a
-replicate SZ _     = Nil
-replicate (SS n) a = a :- replicate n a
+--
+-- basics
+--
+
+length :: Vect n a -> Int
+length Nil      = 0
+length (_ :- v) = 1 + length v
+
+--
+-- mapping
+--
+
+map :: (a -> b) -> Vect n a -> Vect n b
+map _ Nil      = Nil
+map f (a :- v) = f a :- map f v
+
+--
+-- zipping
+--
 
 zipWithSame :: (a -> b -> c) -> Vect n a -> Vect n b -> Vect n c
-zipWithSame f Nil Nil             = Nil
+zipWithSame f Nil       Nil       = Nil
 zipWithSame f (a :- v1) (b :- v2) = f a b :- zipWithSame f v1 v2
 
 zipWith :: (a -> b -> c) -> Vect n a -> Vect m b -> Vect (Min n m) c
-zipWith f Nil Nil             = Nil
-zipWith f Nil (b :- v)        = Nil
-zipWith f (a :- v) Nil        = Nil
+zipWith f Nil       Nil       = Nil
+zipWith f Nil       (b :- v)  = Nil
+zipWith f (a :- v ) Nil       = Nil
 zipWith f (a :- v1) (b :- v2) = f a b :- zipWith f v1 v2
 
 zipSame :: Vect n a -> Vect n b -> Vect n (a, b)
@@ -78,3 +108,36 @@ zipSame = zipWithSame (,)
 
 zip :: Vect n a -> Vect m b -> Vect (Min n m) (a, b)
 zip = zipWith (,)
+
+--
+-- sorting
+--
+
+{-| insertion sort (not efficient on large vectors)
+-}
+sort :: Ord a => Vect n a -> Vect n a
+sort Nil      = Nil
+sort (a :- v) = let sortedv = sort v in insert a sortedv
+
+
+{-| insert an element in a sorted vector
+-}
+insert :: Ord a => a -> Vect n a -> Vect (S n) a
+insert a Nil      = a :- Nil
+insert a (b :- v) = if a <= b then a :- b :- v else b :- insert a v
+
+--
+-- relation to other structures
+--
+
+toList :: Vect n a -> [a]
+toList Nil      = []
+toList (a :- v) = a : toList v
+
+fromList :: SNat n -> [a] -> Maybe (Vect n a)
+fromList SZ     _        = Just Nil
+fromList (SS n) (x : xs) = (x :-) <$> fromList n xs
+fromList _      _        = Nothing
+
+fromList' :: SingI n => [a] -> Maybe (Vect n a)
+fromList' = fromList sing
