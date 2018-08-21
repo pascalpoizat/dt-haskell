55ff36b4501db4d325e905c764e12800d1498824
pascalpoizat <pascal.poizat@lip6.fr>
2018-08-21 21:19:37 +0300

    refactor: extract a set of tests from main

---
+++ src/TestCase1.hs
@@ -0,0 +1,68 @@
+module TestCase1
+where
+
+import Data.Vect
+import Data.Type.Natural
+import           Prelude           (IO, Integer, Maybe (..), String, print,
+                                    pure, putStrLn, ($), (+), (<$>), (>>=))
+--
+-- consider writing real tests using Tasty
+--
+
+type IVect n = Vect n Integer
+type IVect3 = IVect N3
+
+aV1 :: IVect3
+aV1 = 1 :> 2 :> 3 :> Nil
+
+aV2 :: IVect3
+aV2 = 4 :> 5 :> 6 :> Nil
+
+aV3 :: IVect3
+aV3 = replicate sN3 0
+
+aV4 :: IVect3
+aV4 = replicate' 0
+
+aV5 :: Maybe (IVect N6)
+aV5 = fromList' [1,1,3,4,7,11]
+
+testAppend :: IO ()
+testAppend = print $ aV2 ++ aV1
+
+testFromList :: IO ()
+testFromList = case aV5 of
+  Nothing -> print "nothing"
+  Just v  -> print v
+
+testShow :: IO ()
+testShow = do
+  print aV1
+  print aV2
+
+testReplicate :: IO ()
+testReplicate = do
+  print aV3
+  print aV4
+
+testMap :: IO ()
+testMap = print $ (+ 1) <$> aV2 ++ aV1
+
+testSort :: IO ()
+testSort = print $ sort $ aV2 ++ aV1
+
+testZip :: IO ()
+testZip = print $ zipWith (+) aV1 aV2
+
+test :: String -> IO () -> IO ()
+test what f = putStrLn what >>= pure f
+
+testcase :: IO ()
+testcase = do
+  test "show" testShow
+  test "replicate" testReplicate
+  test "append" testAppend
+  test "fromList" testFromList
+  test "map" testMap
+  test "sort" testSort
+  test "zip" testZip
